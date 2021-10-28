c43b66db4c5ece7e17929292ae6afaf8fd6d80cc
Coffeegreeder <Coffeegreeder@gmail.com>
2021-10-28 16:18:19 +0300

    jopa

--- regtr/MainWindow.xaml.cs
+++ regtr/MainWindow.xaml.cs
@@ -21,8 +21,46 @@ namespace regtr
     public partial class MainWindow : Window
     {
         public MainWindow()
+           
         {
             InitializeComponent();
+            
+
+
+        }
+        // при нажатии кнопки регистрации открывается окно регистрации
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window window = new Window();
+            Window2 window2 = new Window2();
+            window.Close();
+            window2.Show();
+
+            //try
+            //{
+            //    hachiuebani123Entities1 entities1 = new hachiuebani123Entities1();
+
+            //    var query = entities1.Avtobus.Select(x => x.Количество_мест);
+            //    foreach (var item in query)
+            //    {
+            //        MessageBox.Show(item.ToString());
+            //    }
+
+            //    MessageBox.Show("База работает");
+
+            //}
+            //catch (Exception)
+            //{
+            //    MessageBox.Show("База не работает");
+            //    throw;
+            //}
+        }
+        private void Button_Click_2(object sender,RoutedEventArgs e)
+        {
+            Window3 window3 = new Window3();
+            window3.Show();
         }
     }
 }
+
+
